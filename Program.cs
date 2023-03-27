fec29157b75f974daeb587ffcd3aa941fcbe9ce5
whoisxnio <enioblvck@gmail.com>
2023-03-27 15:09:12 +0100

    minimal web api template

---
+++ Program.cs
@@ -0,0 +1,25 @@
+
+using Microsoft.OpenApi.Models;
+using PizzaStore.DB;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Let's get started with .NET", Version = "v1"});
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c => {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+});
+
+app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
+app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
+app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
+app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
+
+app.Run();
